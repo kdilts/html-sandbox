8a19abe1df26836954f99ea616e0aa4204af9c6a
Kevin Dilts <kdilts13@gmail.com>
2016-10-11 13:38:01 -0600

    added section 1

--- public_html/index.php
+++ public_html/index.php
@@ -1,10 +1,32 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8">
+		<meta charset="utf-8"/>
 		<title>HTML Sandbox</title>
 	</head>
 	<body>
-		<h1>Hello World!</h1>
+
+		<!-- this is the header -->
+		<header>
+			<h1>Hi! This is my page.</h1>
+		</header>
+
+		<!-- this is the main page content -->
+		<main>
+
+			<!-- begin section 1 -->
+			<section>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt
+					ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+					nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa
+					qui officia deserunt mollit anim id est laborum.</p>
+				<img src="img/cat.jpg" alt="This is supposed to be a cat!"/>
+			</section>
+
+			<section></section>
+			<section></section>
+		</main>
+
 	</body>
 </html>
